e84c64d57885abf87e4f362bbc85105fa1d16d52
IssoufTraore <izilapeste71@gmail.com>
2023-07-14 09:54:38 +0000

    ...

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from '../model/user.model';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {
-
+export class LoginComponent implements OnInit {
+  user = new User();
+  constructor() {}
+  ngOnInit(): void {}
 }
